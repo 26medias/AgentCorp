495cb38c68dd93ae25fdace3665c650c1dc7fb02
Julien Loutre <julien@leap-forward.ca>
2024-10-14 12:11:07 -0400

    Update main.js

--- Corp/Office/main.js
+++ Corp/Office/main.js
@@ -1,6 +1,6 @@
 
 import Office from './Office.js';
-
+//import Agent from '../Agent/Agent.js';
 
 (async () => {
     const project = "moon-lava-structure";
@@ -8,3 +8,33 @@ import Office from './Office.js';
     await office.reset();
     await office.init();
 })();
+
+/*
+// Test
+(async () => {
+    const project = "moon-lava-structure";
+    const workspaceDirectory = `../workspaces/${project}`;
+    const agent_A = new Agent(workspaceDirectory, {
+        "name": "agent_A",
+        "description": "You are a sci-fi writer.",
+        "behavior": ""
+    });
+    const agent_B = new Agent(workspaceDirectory, {
+        "name": "agent_B",
+        "description": "You are a sci-fi writer.",
+        "behavior": ""
+    });
+    const agent_R = new Agent(workspaceDirectory, {
+        "name": "agent_R",
+        "description": ""
+    });
+
+    const workspace = new Workspace();
+    workspace.add(agent_A);
+    workspace.add(agent_B);
+    workspace.add(agent_R);
+})();
+
+class AgentTest extends Agent {
+
+}*/
